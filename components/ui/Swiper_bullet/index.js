06f55624e625f4f03a8e7c778ef353114b028d51
Azis <you@example.com>
2024-06-13 15:02:15 +0600

    merge footer

---
+++ components/ui/Swiper_bullet/index.js
@@ -0,0 +1,1 @@
+export { SwiperBullet } from "./SwiperBullet";
---
+++ pageLayout/MainPage/content/data.js
@@ -0,0 +1,37 @@
+export const ProductList = [
+  {
+    title: "Пылесос Samsung",
+    rating: 3,
+    price: 30100,
+  },
+  {
+    title: "Пылесос Nokia",
+    rating: 4,
+    price: 200,
+  },
+  {
+    title: "Пылесос Iphon",
+    rating: 2,
+    price: 12000,
+  },
+  {
+    title: "Пылесос NVIDIA",
+    rating: 5,
+    price: 5000,
+  },
+  {
+    title: "Пылесос GOOD",
+    rating: 1,
+    price: 8000,
+  },
+  {
+    title: "Пылесос transformer",
+    rating: 3.5,
+    price: 3000,
+  },
+  {
+    title: "Пылесос autoboty",
+    rating: 5,
+    price: 54000,
+  },
+];
